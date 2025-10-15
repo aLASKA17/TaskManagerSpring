8b64b99b6acacedab20699c706650da79245b31b
a1aska17 <92105945+aLASKA17@users.noreply.github.com>
2025-10-15 07:55:27 +0600

    Add class Task for task

---
+++ src/main/java/com/example/TaskManager/Task.java
@@ -0,0 +1,47 @@
+package com.example.TaskManager;
+
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private boolean completed;
+
+    Task(int id, String title, String description, boolean completed) {
+        this.title = title;
+        this.id = id;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
--- src/main/java/com/example/TaskManager/TaskManagerApplication.java
+++ src/main/java/com/example/TaskManager/TaskManagerApplication.java
@@ -1,19 +1,32 @@
 package com.example.TaskManager;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
 
-	@RequestMapping("/")
-	public String Task() {
-		return "home";
+	List<Task> taskList = new ArrayList<>();
+
+	@GetMapping("/")
+	public List<Task> getMethodName(@RequestParam String param) {
+		taskList.add(new Task(0, "param", "param", false));
+		return taskList;
 	}
+	
+	
 
 }
